c9a1dac66e8a99287f7735a4274f23fd10d29675
Ram mohan <ramamohanraobotla12@gmail.com>
2024-08-02 19:43:47 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
